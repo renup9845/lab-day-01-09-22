5b370e9284580163727517dee4388ef77c713f9e
renup9845 <110659991+renup9845@users.noreply.github.com>
2022-09-01 20:43:52 +0530

    Create laptop.java

---
+++ laptop.java
@@ -0,0 +1,22 @@
+package com.bankApp;
+
+public class laptop {
+
+
+    int eid;
+    String eCompany;
+    String eName;
+    int ePrice;
+    int eModel;
+   
+    
+    public laptop(int eid, String eCompany,String eName,int ePrice,int eModel){
+        
+        this. eid = eid;
+        this.eCompany = eCompany;
+        this.eName = eName;
+        this.ePrice = ePrice;
+        this.eModel = eModel;
+ }
+
+}
